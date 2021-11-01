3b55e1cdfbcfd3ca938db5ad82b08a1d48484e6c
randol696 <randol696@live.com>
2021-10-31 23:16:45 -0500

    style1

---
+++ movil/web.js
@@ -0,0 +1,12 @@
+function openNav() {
+    document.getElementById("mysidebar").style.width = "250px";
+    document.getElementById("navbar").style.marginLeft = "250px";
+
+}
+
+function closeNav() {
+    document.getElementById("mysidebar").style.width = "0px";
+    document.getElementById("navbar").style.marginLeft = "0px";
+
+}
+
